b5e6cb9c9006ee1f9a507b57b988cbe788a4032b
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 00:47:47 +0300

    Add SSL requirement to database connection pool

--- database/db.py
+++ database/db.py
@@ -3,5 +3,6 @@ import asyncpg
 
 async def create_pool():
     return await asyncpg.create_pool(
-        dsn=os.getenv("DATABASE_URL")
+        dsn=os.getenv("DATABASE_URL"),
+        ssl="require"
     )

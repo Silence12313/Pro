03eb12b9b666bdd477559686685109b400065996
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 00:13:21 +0300

    Add DSN parameter to create_pool function

--- database/db.py
+++ database/db.py
@@ -6,6 +6,7 @@ async def create_pool():
 
     return await asyncpg.create_pool(
         user=os.getenv("DB_USER"),
+        dsn=os.getenv("DATABASE_URL"),
         password=os.getenv("DB_PASS"),
         database=os.getenv("DB_NAME"),
         host=os.getenv("DB_HOST")

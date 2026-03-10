23e694f6c6fed2b63be689dad0325b294884398f
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 00:47:10 +0300

    Refactor db.py to remove unused import
    Removed unnecessary imports and cleaned up the code.

--- database/db.py
+++ database/db.py
@@ -1,13 +1,7 @@
-import asyncpg
 import os
-
+import asyncpg
 
 async def create_pool():
-
     return await asyncpg.create_pool(
-        user=os.getenv("DB_USER"),
-        dsn=os.getenv("DATABASE_URL"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME"),
-        host=os.getenv("DB_HOST")
+        dsn=os.getenv("DATABASE_URL")
     )

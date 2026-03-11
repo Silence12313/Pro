22581aa6ce7e3889982025d04c4fe8275182d31a
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 14:54:46 +0300

    Add blank line before create_pool function

--- database/db.py
+++ database/db.py
@@ -1,8 +1,10 @@
 import os
 import asyncpg
 
+
 async def create_pool():
+
     return await asyncpg.create_pool(
-        dsn=os.getenv("DATABASE_URL"),
+        dsn=os.getenv("DATABASE_URL")
         ssl="require"
     )

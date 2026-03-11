8ebd67387ee8dc569d77f7cb16240a17d198e3e8
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 16:02:24 +0300

    Implement database connection pool management
    Added functions to create and retrieve a database connection pool.

--- database/db.py
+++ database/db.py
@@ -1,11 +1,20 @@
 import os
 import asyncpg
 
+pool = None
+
 
 async def create_pool():
+
+    global pool
 
     pool = await asyncpg.create_pool(
         dsn=os.getenv("DATABASE_URL")
     )
+
+    return pool
+
+
+def get_pool():
 
     return pool

7e7285a578050b248bba6ad612e13843b124620b
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 15:55:34 +0300

    Refactor create_pool to assign pool variable

--- database/db.py
+++ database/db.py
@@ -4,7 +4,8 @@ import asyncpg
 
 async def create_pool():
 
-    return await asyncpg.create_pool(
+    pool = await asyncpg.create_pool(
         dsn=os.getenv("DATABASE_URL")
-        ssl="require"
     )
+
+    return pool

a7c2c168a7832d28c3454da152cb41ef0d10cfa3
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 14:59:29 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -7,13 +7,21 @@ async def init_db(pool):
         CREATE TABLE IF NOT EXISTS users (
 
             id SERIAL PRIMARY KEY,
+
             telegram_id BIGINT UNIQUE,
+
             first_name TEXT,
-            referral_code TEXT,
+
+            referral_code TEXT UNIQUE,
+
             invited_by TEXT,
+
             invited_count INTEGER DEFAULT 0,
+
             subscribed BOOLEAN DEFAULT FALSE,
+
             bonus_received BOOLEAN DEFAULT FALSE,
+
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
         )

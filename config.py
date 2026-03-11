ffa19cdfc055f4c875aeb1ec3c5373c4ab983f44
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-11 14:53:56 +0300

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -1,30 +1,23 @@
 import os
 
-# Telegram
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-BOT_USERNAME = os.getenv("BOT_USERNAME")
+
 
 TELEGRAM_REF_LINK = f"https://t.me/{BOT_USERNAME}?start=" if BOT_USERNAME else None
 BASE_REF_URL = os.getenv("BASE_REF_URL") or TELEGRAM_REF_LINK
 # MAX
 MAX_TOKEN = os.getenv("MAX_TOKEN")
 
-# Канал
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "your_art_muse")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Реферальная система
-REFERRALS_REQUIRED = int(os.getenv("REFERRALS_REQUIRED", 3))
+BOT_USERNAME = os.getenv("BOT_USERNAME")
 
-# бонус
-BONUS_URL = os.getenv(
-    "BONUS_URL",
-    "https://www.youtube.com/playlist?list=PL2DjtAFoLP6w3ztMXg4eLzBPUj3iaFvPm"
-)
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 
-CONSULT_URL = os.getenv(
-    "CONSULT_URL",
-    "https://onstudy.org/konsultatsiya-art/"
-)
+REFERRALS_REQUIRED = 3
+
+BONUS_LINK = "https://www.youtube.com/playlist?list=PL2DjtAFoLP6w3ztMXg4eLzBPUj3iaFvPm"
+
+CONSULT_LINK = "https://onstudy.org/konsultatsiya-art/"
 
 # администратор
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
